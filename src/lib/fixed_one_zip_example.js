6aa58432171d1e17d7e4867c753b6e33a055f674
kkgthb <44843658+kkgthb@users.noreply.github.com>
2021-11-04 11:44:37 -0500

    Fixed single-fixed-example to be API-based

--- src/lib/fixed_one_zip_example.js
+++ src/lib/fixed_one_zip_example.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 module.exports = async function (which_zip_doesnt_matter) {
   let raw_data_fixed = {
     "post code": "65806",
@@ -13,9 +15,11 @@ module.exports = async function (which_zip_doesnt_matter) {
       },
     ],
   };
+  let url = `https://api.zippopotam.us/us/65807`
+  const raw_data_downloaded = await fetch(url).then((data) => data.json());
   let the_data = {};
-  the_data["post_code"] = raw_data_fixed["post code"];
-  the_data["place_name"] = raw_data_fixed["places"][0]["place name"];
+  the_data["post_code"] = raw_data_downloaded["post code"];
+  the_data["place_name"] = raw_data_downloaded["places"][0]["place name"];
   //console.error(JSON.stringify(the_data));
   return the_data;
 };
